f5605acd0c4f103e5071be3d61bdb1bf2c1eb301
Wagner de Lima <waglds@gmail.com>
2021-12-24 17:27:25 +0100

    feat: add test view. check access token works on view.

--- facebook_setup/urls.py
+++ facebook_setup/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import re_path
 
+from facebook_setup.views import MyView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    re_path('read/', MyView.as_view(), name='read'),
 ]
